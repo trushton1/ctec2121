62e60ca8a7e3bad69a5d58a33f0a07c270304e90
Thomas Rushton <P1322523X@GH584-007.lec-admin.dmu.ac.uk>
2015-01-05 12:40:17 +0000

    Created program and included addition function

---
+++ operations.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main() {
+	
+	int a = 5, b = 5;
+	char c;
+	
+	printf("Please select one of the four basic mathematical operations...\n");
+	scanf("%c", &c);
+	
+	switch (c) {
+		case '+': printf("a %c b = %i", c, addition(a,b));
+			break;
+		default : printf("INVALID CHOICE");
+			break;
+	}
+	
+	printf("\n\n");
+}
+
+int addition(a, b) {
+return a + b;
+}
